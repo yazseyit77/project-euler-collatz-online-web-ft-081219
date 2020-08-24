f2090108e6e7266ccaa5ab7017f7b2eb85f038a1
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-24 23:58:00 +0000

    Done.

--- lib/project_euler_collatz.rb
+++ lib/project_euler_collatz.rb
@@ -1,14 +1,37 @@
 def even_next(n)
+  n / 2
 end
 
 def odd_next(n)
+  3 * n + 1
 end
 
 def next_value(n)
+  if n.even?
+    even_next(n)
+  else
+    odd_next(n)
+  end
 end
 
 def collatz(n)
+  sequence = [n]
+  while n != 1
+    n = next_value(n)
+    sequence.push(n)
+  end
+  sequence
 end
 
 def longest_collatz
+  longest = 0
+  result = nil
+
+  (800000..1000000).each do |n|
+    if(collatz(n).length > longest)
+      longest = collatz(n).length
+      result = n
+    end
+  end
+  result
 end
